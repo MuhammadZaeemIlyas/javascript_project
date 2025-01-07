1595973d1ebf909ab5b14e9189e960309c9e18db
Muhammad Zaeem <zaeem@dynamicsoft.biz>
2025-01-07 17:44:56 +0500

    Script.js changes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-a = {
-    name: 'assvgg',
-    age: '21',
-
-}
-console.log(a.age);
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    height: "120px",
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        start: "top -10%",
+        markers: true
+    }
+}) 
